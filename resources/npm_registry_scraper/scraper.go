a72356b6b98dee84668e6e4283b53d02432498ca
Frederic Sauve Hoover <rsauveho@ualberta.ca>
2019-04-01 04:20:26 -0600

    new name stuff

--- resources/npm_registry_scraper/scraper.go
+++ resources/npm_registry_scraper/scraper.go
@@ -1,71 +1,36 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
 	"sync"
 )
+
+var ES_BASE_URL = "http://localhost:9200/packages/_package/"
+var REGISTRY_BASE_URL = "https://registry.npmjs.org/"
 
 var wg = &sync.WaitGroup{}
 var sem = make(chan int, 100)
 
-// Our datastructures for JSON unmarshalling
-type Data struct {
-	Objects []PackageObj `json:"objects"`
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
 
-type PackageObj struct {
-	Package struct {
-		Name string `json:"name"`
-	} `json:"package"`
-}
-
-func get_package_names(stable bool, offset int) {
-	defer wg.Done()
-
-	stability := ""
-	if stable {
-		stability = "stable"
-	} else {
-		stability = "unstable"
-	}
-
-	// do an http GET
-	urlstr := fmt.Sprintf("https://registry.npmjs.org/-/v1/search?text=is:%s&size=250&from=%d", stability, offset)
-	resp, err := http.Get(urlstr)
-	if err != nil {
-		fmt.Println(err)
-	}
-
+func get_package_info(name string) {
+	// We want to pull from registry
+	resp, err := http.Get(REGISTRY_BASE_URL + name)
+	check(err)
 	defer resp.Body.Close()
 
-	// read the response from the http request
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println(err)
-	}
+	// send to our ES server
+	contents, err := ioutil.ReadAll(resp.Body)
 
-	// Handle the unjsonifying data
-	data := Data{}
-	json.Unmarshal(body, &data)
-
-	// create the file to write to
-	f, err := os.OpenFile(fmt.Sprintf("/Volumes/RFSH-Ext/663-Names/%s-%d.txt", stability, offset), os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	defer f.Close()
-
-	// print all the names to our file
-	for _, p := range data.Objects {
-		fmt.Fprintf(f, "%s\n", p.Package.Name)
-	}
-
-	<-sem
+	werr := ioutil.WriteFile("/Volumes/RFSH-Ext/ESData"+name, contents, 0644)
+	check(werr)
+	//<-sem
 }
 
 func main() {
@@ -76,10 +41,7 @@ func main() {
 	 Queries are split into chunks of 250 packages since npm registry has that as it's
 	 maximum search query size
 	*/
-	for i := 0; i < 4197; i += 250 {
-		wg.Add(1)
-		go get_package_names(false, i)
-	}
+	get_package_info("isnum")
 	//for i := 0; i < 419727; i += 250 {
 	//sem <- 1
 	//wg.Add(1)
